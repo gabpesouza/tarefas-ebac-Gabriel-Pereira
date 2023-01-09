f152cff9235a4ab1007f808ced89d9b2cc8713e6
Gabriel Pereira <gabriel.psouza1997@outlook.com>
2023-01-09 20:20:19 -0300

    atividade modulo 8 - calculo media

---
+++ src/br/com/ebac/mod8/CalculoMedia.java
@@ -0,0 +1,18 @@
+package br.com.ebac.mod8;
+
+public class CalculoMedia {
+
+	public static void main(String[] args) {
+		
+		double nota1 = 6.0;
+		double nota2 = 6.5;
+		double nota3 = 9.3;
+		double nota4 = 8.2;
+		
+		double media = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		System.out.printf("Valor da media final: %2f", media);
+		
+	}
+		
+}
